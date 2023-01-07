7c2fc0d6149280cb3193f726e35218730bab68bb
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2023-01-06 22:30:34 -0500

    feat: add mobile menu (without own styles)

--- src/layouts/scroll-top/index.tsx
+++ src/layouts/scroll-top/index.tsx
@@ -1,12 +1,10 @@
 import './_styles.scss';
 
 export const ScrollTop: React.FC = (): JSX.Element => {
+	// TODO: add sroll-top functionallity
 	return (
-		<>
-			{/* TODO: add sroll-top functionallity*/}
-			<button id="scroll-top" title="Back to Top">
-				<i className="icon-arrow-up"></i>
-			</button>
-		</>
+		<button id="scroll-top" title="Back to Top">
+			<i className="icon-arrow-up"></i>
+		</button>
 	);
 };
